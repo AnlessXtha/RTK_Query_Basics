46dfdf0f3f75937f099090df077854296ae2e7b3
AnlessXtha <aadesh.shrrstha@gmail.com>
2023-11-22 11:40:44 +0545

    Completed the project in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 function App() {
+  // Using a query hook automatically fetches data and returns query value
+  const { data, error, isLoading } = useGetPokemonByNameQuery("charizard");
+  // Indvidual hooks are also accessible under the generate endpoints:
+  // const { data, error, isLoading} = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
     </div>
   );
 }
